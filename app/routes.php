c53319fd087ac1b6a9da9c3d2c9f9385b87ea4e2
David Dorr <shishkebab101@hotmail.com>
2016-11-27 18:21:12 +0200

    Inital Commit,... together with some log in features and basic sql migrations.

---
+++ app/routes.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+// route for the register page
+
+Route::get('/register', function()
+{
+	return View::make('register');
+});
+
+Route::post('/register', function()
+{
+	$user = new User;
+	$user->email = Input::get('email');
+	$user->username = Input::get('username');
+	$user->password = Hash::make(Input::get('password'));
+	$user->save();
+	$theEmail = Input::get('email');
+	return View::make('thanks')->with('theEmail', $theEmail);
+});
